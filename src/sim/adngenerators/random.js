c832fa7e93bc31b398d63ddcbfae6496fac0f52c
Thibaut REMY <thibaut@domora.com>
2015-04-23 15:57:10 +0200

    Bigger wheels

--- src/sim/adngenerators/random.js
+++ src/sim/adngenerators/random.js
@@ -3,10 +3,10 @@ ADNGenerators.random = function () {
 
     adn.props["W1"] = true;
     adn.props["W2"] = true;
-    adn.props["W1R"] = Math.random()*35 + 5; // 5 < . < 40
-    adn.props["W2R"] = Math.random()*35 + 5;
-    adn.props["W1A"] = Math.random()*4;
-    adn.props["W2A"] = Math.random()*4;
+    adn.props["W1R"] = Math.random()*30 + 15; // 5 < . < 40
+    adn.props["W2R"] = Math.random()*30 + 15;
+    adn.props["W1A"] = Math.random();
+    adn.props["W2A"] = Math.random();
 
     return adn;
 };
